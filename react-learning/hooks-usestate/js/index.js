11736f620332f422d6b1bb99b1fc622b91709217
Alex Rafter <alexrafteremail@gmail.com>
2022-05-09 01:23:56 +0100

    mesing about with diff esm builds inc a dev version, and experimenting with hooks

--- react-learning/hooks-usestate/js/index.js
+++ react-learning/hooks-usestate/js/index.js
@@ -1,30 +1,53 @@
-import React from "https://cdn.skypack.dev/react@17.0.1";
-import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+'use strict';
+// import React from "https://cdn.skypack.dev/react";
+// import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+import { React, ReactDOM } from 'https://unpkg.com/es-react/dev';
+// https://www.npmjs.com/package/es-react
+
 import reactHyperscriptHelpers from 'https://cdn.skypack.dev/react-hyperscript-helpers';
 const { div, h1, h2, h3, button, ul, li, h } = reactHyperscriptHelpers
 
-class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+const App = () => {
+    const [text, setText] = React.useState('Hello Prev World!')
+    const [count, setCount] = React.useState(0);
+    // Similar to componentDidMount and componentDidUpdate:
+    React.useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+    });
 
-        this.state = {
-            name: 'John'
-        };
-        
-        this.tick = this.tick.bind(this);
+
+    const objOfEVents = {
+        // onMouseOver: () => setText('You have scrolled over me'),
+        // onMouseOut: () => setText('Bye, you have scrolled out')
     }
 
-    tick() {
-        this.setState({
-            name: (this.state.name === 'Tommy') ? 'John' : 'Tommy'
-        });
+    const btnEvents = {
+        title : 0,
+        onClick: (e) => {
+            // console.log(e.target.title)
+            // if (e.target.title >= 2) 
+            setText('more than nine button clicks')
+
+            // setCount(count + 1)
+            // e.target.title++
+        } 
     }
+
+    let n = 0
+    const x = () => {
+        if (n > 0) return
+        setText('more than nine button clicks')
+        n++
+    }
+
+    if (count === 3) x()
     
-    render() {
-        // With HyperScript Helpers
-        return button('.btn.btn-primary', { onClick: this.tick }, this.state.name)
-    }
+
+    const Button = () => button('.btn.btn-primary', btnEvents, `Clicked ${count} times`)
+
+    return div([h1('.display-1', objOfEVents, text,), Button()])
 }
 
 const domContainer = document.querySelector('#root');
