e7fc15ccf501834f6697b07f37dadaf45a9f99b2
Alex Rafter <alexrafteremail@gmail.com>
2022-05-14 20:30:17 +0100

    es build with react - working

--- react-learning/es-react-test/src/js/index.jsx
+++ react-learning/es-react-test/src/js/index.jsx
@@ -1,61 +1,44 @@
 import React from "react";
 import { useState, createContext, useContext } from "react";
 import ReactDOM from "react-dom/client";
+import parse from 'html-react-parser';
+import { marked } from 'marked';
 
-const UserContext = createContext();
+let url = 'https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/testForWeb/markdown';
+// let Data
+// const setData = (x) => Data = x;
+
+
+fetch(url)
+  .then((response) => response.json())
+  .then(json => {
+
+    const Data = createContext(json.markdown[0].content);
+
+    function Component1() {
+
+      const dPassed = useContext(Data);
+      return <div>{parse(dPassed)}</div>
+    }
+
+    // App
+    // const App = () => {
+    //   return (
+    //     <Data.Provider>
+    //       <h1>Hello</h1>
+    //     </Data.Provider>
+    //   );
+    // }
+    // App
+
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<Component1 />);
+  });
+
 
 // TST
-function Component1() {
-  const [user, setUser] = useState("Just Rikki...");
-
-  return (
-    <UserContext.Provider value={user}>
-      <h1>{`Hello ${user}`}</h1>
-      <Component2 user={user} />
-    </UserContext.Provider>
-  );
-}
 
 // Test
 // And another
 // And  third
-function Component2() {
-  return (
-    <>
-      <h1>Component 2</h1>
-      <Component3 />
-    </>
-  );
-}
-
-function Component3() {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <Component4 />
-    </>
-  );
-}
-
-function Component4() {
-  return (
-    <>
-      <h1>Component 4</h1>
-      <Component5 />
-    </>
-  );
-}
-
-function Component5() {
-  const user = useContext(UserContext);
-
-  return (
-    <>
-      <h1>Component 5</h1>
-      <h2>{`Hello ${user.length} again!`}</h2>
-    </>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Component1 />);
