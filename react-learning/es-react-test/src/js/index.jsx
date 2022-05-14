1e4155fcc146aeb1a696c50c4de06f3890ae4b51
Alex Rafter <alexrafteremail@gmail.com>
2022-05-14 21:08:58 +0100

    nearly there with preact swap out

--- react-learning/es-react-test/src/js/index.jsx
+++ react-learning/es-react-test/src/js/index.jsx
@@ -1,44 +1,24 @@
-import React from "react";
-import { useState, createContext, useContext } from "react";
-import ReactDOM from "react-dom/client";
-import parse from 'html-react-parser';
+import { h, render, Component, createContext } from 'preact';
+import { useContext } from 'preact/hooks';
+import parse from 'preact-parser';
+// import parse from 'html-react-parser';
 import { marked } from 'marked';
 
 let url = 'https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/testForWeb/markdown';
-// let Data
-// const setData = (x) => Data = x;
-
 
 fetch(url)
   .then((response) => response.json())
   .then(json => {
 
     const Data = createContext(json.markdown[0].content);
+    console.log(Data.__)
 
     function Component1() {
-
-      const dPassed = useContext(Data);
-      return <div>{parse(dPassed)}</div>
+      return <p>{parse(marked.parse(Data))}</p>
     }
 
-    // App
-    // const App = () => {
-    //   return (
-    //     <Data.Provider>
-    //       <h1>Hello</h1>
-    //     </Data.Provider>
-    //   );
-    // }
-    // App
 
-
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(<Component1 />);
+    render(<Component1 />, document.body)
   });
 
 
-// TST
-
-// Test
-// And another
-// And  third
