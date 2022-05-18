09d272706bed913e65e5814295cd13992c6b67cd
Alex Rafter <alexrafteremail@gmail.com>
2022-05-18 22:44:23 +0100

    updated md blog mvp unfin

--- react-learning/es-react-test/src/js/index.jsx
+++ react-learning/es-react-test/src/js/index.jsx
@@ -1,24 +1,29 @@
 import { h, render, Component, createContext } from 'preact';
 import { useContext } from 'preact/hooks';
-import parse from 'preact-parser';
-// import parse from 'html-react-parser';
+import { parse } from 'preact-parser';
 import { marked } from 'marked';
+
 
 let url = 'https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/testForWeb/markdown';
 
 fetch(url)
   .then((response) => response.json())
   .then(json => {
+    // console.log(json.markdown[0].content)
 
     const Data = createContext(json.markdown[0].content);
-    console.log(Data.__)
 
-    function Component1() {
-      return <p>{parse(marked.parse(Data))}</p>
+    function Markdown({className}) {
+      const MD = marked.parse(useContext(Data))
+      const HTML = parse(MD)
+      return <div id="topLevel" className={className}>{HTML}</div>
     }
 
-
-    render(<Component1 />, document.body)
+    function Component1() {
+      return <Markdown className="tst"/>
+    }
+const root = document.querySelector('#root')
+    render(<Component1 />, root)
   });
 
 
