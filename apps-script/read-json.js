4558e5cd14e912778e2cfc2dd67785f1863d8275
Alex Rafter <alexrafteremail@gmail.com>
2022-05-22 11:02:07 +0100

    tweaks for GAS

--- apps-script/read-json.js
+++ apps-script/read-json.js
@@ -1,15 +1,21 @@
-const json = require('./dl-full.json')
-const descs = json.filter(onlyBlogsToPublish)
+function main() {
 
-function onlyBlogsToPublish(item) {
-    // vars
-    const isSetToPublish = item.description.includes('#na_publish')
-    let isBlogMD = false
-    const filenames = item.files
-    // actions
-    const checkIfBlog = filename => filename.includes('blog.md') && filename
-    for (const filename in filenames) if (checkIfBlog(filename)) isBlogMD = true
-    if (isSetToPublish && isBlogMD) return item
+    const fetchGists = () => UrlFetchApp.fetch('https://api.github.com/users/Alex-Rafter/gists')
+    const json = JSON.parse(fetchGists())
+    const blogsToPublish = json.filter(onlyReturnBlogsToPublish)
+
+    function onlyReturnBlogsToPublish(item) {
+        // vars
+        let [isBlogMD, isSetToPublish] = [false, false]
+        const filenames = item.files
+        // actions
+        isSetToPublish = item.description.includes('#na_publish')
+        const checkIfBlog = filename => filename.includes('blog.md') && filename
+        for (const filename in filenames) if (checkIfBlog(filename)) isBlogMD = true
+        if (isSetToPublish && isBlogMD) return item
+    }
+
+    console.log(blogsToPublish)
 }
 
-console.log(descs)
+main()
