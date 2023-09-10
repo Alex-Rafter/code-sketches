11feff5b4739c3cd59582156acc5e7b95d0b2c33
Alex Rafter <alexrafteremail@gmail.com>
2023-09-10 16:31:19 +0100

    closure experiments 4

--- quokka/closure-intro.js
+++ quokka/closure-intro.js
@@ -1,6 +1,5 @@
 // Messing about with little closure exercises, after reading closure into in Eloquent JS Book.
 
-
 // 1 Counter Function
 function createCounter() {
     let i = 1
@@ -13,7 +12,7 @@ console.log(counter())
 console.log(counter())
 console.log(counter())
 
-//2 Multiplier Function
+// 2 Multiplier Function
 
 function createMultiplier(factorOne, factorTwo) {
     return (arg) => (factorOne * factorTwo) * arg
@@ -33,7 +32,7 @@ console.log(msg('hello'))
 // 4 Array accumulator
 
 function arrayAccumulator() {
-    let arr = []
+    const arr = []
     return arg => arr.push(arg) && arr
 }
 
@@ -42,11 +41,10 @@ console.log(accumulator(1))
 console.log(accumulator(2))
 console.log(accumulator(7))
 
-
 // 5 Event Emitter
 
 function createEmitter() {
-    let eventListeners = {}
+    const eventListeners = {}
     return {
         on(event, cb) {
             eventListeners[event] = cb
@@ -58,45 +56,35 @@ function createEmitter() {
 }
 
 const myEmitter = createEmitter()
-myEmitter.on("click", () => console.log("test"))
-myEmitter.on("hover", () => console.log("hovered"))
-myEmitter.emit("click")
-myEmitter.emit("hover")
+myEmitter.on('click', () => console.log('test'))
+myEmitter.on('hover', () => console.log('hovered'))
+myEmitter.emit('click')
+myEmitter.emit('hover')
 
-// Rate Limiter
+// 6 Rate Limiter
 
 function rateLimiter(cb, time) {
-    let x = false
-    setTimeout(() => {
-        x = true
-        console.log("testt", x)
-    }, time)
-    return () => x && cb()
-    // setTimeout()
-    // return () => cb()
-    // let x = true
-    // const x = () => () => cb()
-    // setTimeout(() => {
-    //     y()
-    // }, time)
-    // function y() {
-    //     return () => cb()
-    // }
-    // if (x = false) {
-    //     return () => cb()
-    // }
-    // while (x = true) { }
+    let blockFunc = false
+
+    function unfreezeAfterLimit() {
+        setTimeout(() => {
+            blockFunc = false
+        }, time)
+    }
+
+    return () => {
+        if (!blockFunc) {
+            cb()
+            blockFunc = true
+            unfreezeAfterLimit()
+        }
+    }
 
 }
 
-const limitedFunc = rateLimiter(() => console.log("limited click"), 1000)
+const limitedFunc = rateLimiter(() => console.log('limited click'), 1000)
 limitedFunc()
 limitedFunc()
 limitedFunc()
-limitedFunc()
-limitedFunc()
-
-
-
-
-
+setTimeout(limitedFunc, 2000)
+setTimeout(limitedFunc, 4000)
