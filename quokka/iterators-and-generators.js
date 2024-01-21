819e6d12cf60cdf9f4d1974952dfada9bdb8a4fa
Alex Rafter <alexrafteremail@gmail.com>
2024-01-21 21:52:52 +0000

    iterators cont

--- quokka/iterators-and-generators.js
+++ quokka/iterators-and-generators.js
@@ -1,57 +1,132 @@
-import { h } from "preact";
-import htm from "htm";
-import register from "preact-custom-element";
-const html = htm.bind(h);
+import { createApp, reactive } from 'petite-vue'
+// import { createApp, ref } from 'vue'
 
 
-export function Fieldset({ title }) {
-    const state = {
-        1: false,
-        2: false,
-        3: false
-    }
+console.log("iterators-and-generators.js");
 
-    function* formWizard() {
-        let step1 = yield showStep1();
-        let step2 = yield showStep2(step1);
-        let step3 = yield showStep3(step2);
-        submitFinalData(step3);
+function Title() {
+    return {
+        init(el) {
+            console.log("inner init", this, el);
+        },
+        title: 'I am some text!!',
+        $template: /*html*/`
+            <h1>{{ title }}</h1>
+            <button @click="store.formWizard.next()">Next!</button>
+        `
     }
-
-    const wizard = formWizard();
-    wizard.next();
-
-    function nextStep() {
-        wizard.next();
-    }
-    function showStep1() {
-
-        this.setState({ [1]: true })
-        console.log("showStep1", stepsToShow);
-    }
-    function showStep2() {
-        this.setState({ [2]: true })
-        console.log("showStep2", stepsToShow);
-    }
-    function showStep3() {
-        this.setState({ [3]: true })
-        console.log("showStep3", stepsToShow);
-    }
-    function submitFinalData() {
-        console.log("submitFinalData", stepsToShow);
-    }
-    return html`
-    <style>
-        .red {
-            color: red;
-        }
-    </style>
-    <fieldset>
-        <legend class=${stepsToShow[2] && 'red'}>${title}</legend>
-        <input type="text" name="name" />
-        <button onClick=${nextStep}>Next</button>
-    </fieldset>
-  `;
 }
 
-register(Fieldset, "ar-fieldset", ["name"], { shadow: true });
+function Test() {
+    return {
+        title: 'I am a test!!',
+        $template: /*html*/`
+            <h1>{{ title }}</h1>
+            <button @click="store.formWizard.next()">Onwards!</button>
+        `
+    }
+}
+
+
+function Fieldset() {
+    return {
+        logger() {
+            console.log("logger", this.stepsToShow);
+        },
+        title: 'I am some text',
+        *formWizard() {
+            console.log("formWizard", this.stepsToShow);
+            let step1 = yield this.showStep1();
+            console.log("formWizard", step1);
+            let step2 = yield this.showStep2(step1);
+            console.log("formWizard", step2);
+            let step3 = yield this.showStep3(step2);
+            console.log("formWizard", step3);
+            this.submitFinalData(step3);
+
+        },
+        formWizardResult: null,
+        // init() {
+        //     this.formWizardResult = this.formWizard.bind(this)();
+        // },
+        showStep1() {
+            this.title = "Step 1";
+        },
+        showStep2() {
+            this.title = "Step 2";
+        },
+        showStep3() {
+            this.title = "Step 3";
+        },
+        submitFinalData() {
+            // this.title = "Final";
+            this.$template = this.altTemplate;
+        },
+        test() {
+            console.log("test");
+        },
+
+        altTemplate: `<div>Next To Final</div>
+        <button @click="test">Test</button>
+        `,
+        altTemplateTwo: `<div>Final</div>`,
+        renderedTemplate: ``,
+        init(el) {
+            console.log("test init", this, el);
+            setTimeout(() => {
+                console.log("setTimeout");
+                this.renderedTemplate = this.altTemplate;
+            }, 2000);
+            setTimeout(() => {
+                console.log("setTimeout");
+                this.renderedTemplate = this.altTemplateTwo;
+            }, 4000);
+        },
+        t1: true,
+        t2: false,
+        $template: /*html*/`
+            <div v-if="store.t2" @vue:mounted="init($el)" v-scope="Test()"></div>
+            <div v-else-if="store.t1"  @vue:mounted="init($el)" v-scope="Title()"></div>
+            <!-- <div @vue:mounted="init($el)" v-html="renderedTemplate"></div> -->
+                `
+    }
+}
+
+const store = reactive({
+    init() {
+        function* gen(thisArg) {
+            console.log("formWizard 1", thisArg);
+            yield thisArg.t2 = true;
+            console.log("formWizard 2", thisArg);
+            yield thisArg.t2 = false;
+            // console.log("formWizard", step1);
+            // let step2 = yield this.showStep2(step1);
+            // console.log("formWizard", step2);
+            // let step3 = yield this.showStep3(step2);
+            // console.log("formWizard", step3);
+            // this.submitFinalData(step3);
+
+        }
+        this.formWizard = gen(this);
+    },
+    t1: true,
+    t2: false,
+    // *formWizard() {
+    //     console.log("formWizard", this.stepsToShow);
+    //     let step1 = yield this.showStep1();
+    //     console.log("formWizard", step1);
+    //     let step2 = yield this.showStep2(step1);
+    //     console.log("formWizard", step2);
+    //     let step3 = yield this.showStep3(step2);
+    //     console.log("formWizard", step3);
+    //     this.submitFinalData(step3);
+
+    // },
+})
+
+store.init()
+
+
+
+createApp({ store, Title, Fieldset, Test }).mount('#root')
+
