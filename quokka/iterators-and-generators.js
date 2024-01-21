e1cf842aea51eaa4a011fb26222d6bf6c2aaabf0
Alex Rafter <alexrafteremail@gmail.com>
2024-01-21 21:54:51 +0000

    iterators cont 2

--- quokka/iterators-and-generators.js
+++ quokka/iterators-and-generators.js
@@ -19,6 +19,9 @@ function Title() {
 
 function Test() {
     return {
+        init(el) {
+
+        },
         title: 'I am a test!!',
         $template: /*html*/`
             <h1>{{ title }}</h1>
@@ -30,60 +33,6 @@ function Test() {
 
 function Fieldset() {
     return {
-        logger() {
-            console.log("logger", this.stepsToShow);
-        },
-        title: 'I am some text',
-        *formWizard() {
-            console.log("formWizard", this.stepsToShow);
-            let step1 = yield this.showStep1();
-            console.log("formWizard", step1);
-            let step2 = yield this.showStep2(step1);
-            console.log("formWizard", step2);
-            let step3 = yield this.showStep3(step2);
-            console.log("formWizard", step3);
-            this.submitFinalData(step3);
-
-        },
-        formWizardResult: null,
-        // init() {
-        //     this.formWizardResult = this.formWizard.bind(this)();
-        // },
-        showStep1() {
-            this.title = "Step 1";
-        },
-        showStep2() {
-            this.title = "Step 2";
-        },
-        showStep3() {
-            this.title = "Step 3";
-        },
-        submitFinalData() {
-            // this.title = "Final";
-            this.$template = this.altTemplate;
-        },
-        test() {
-            console.log("test");
-        },
-
-        altTemplate: `<div>Next To Final</div>
-        <button @click="test">Test</button>
-        `,
-        altTemplateTwo: `<div>Final</div>`,
-        renderedTemplate: ``,
-        init(el) {
-            console.log("test init", this, el);
-            setTimeout(() => {
-                console.log("setTimeout");
-                this.renderedTemplate = this.altTemplate;
-            }, 2000);
-            setTimeout(() => {
-                console.log("setTimeout");
-                this.renderedTemplate = this.altTemplateTwo;
-            }, 4000);
-        },
-        t1: true,
-        t2: false,
         $template: /*html*/`
             <div v-if="store.t2" @vue:mounted="init($el)" v-scope="Test()"></div>
             <div v-else-if="store.t1"  @vue:mounted="init($el)" v-scope="Title()"></div>
